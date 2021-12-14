e1807b89a91938aa609352a473a5381bc5b67e88
TrygviZL <56014816+TrygviZL@users.noreply.github.com>
2021-12-14 17:43:06 +0100

    moved lambda and firehose to construct

--- lib/coin_cap-firehose-s3-rds-stack.ts
+++ lib/coin_cap-firehose-s3-rds-stack.ts
@@ -7,115 +7,19 @@ import * as iam from '@aws-cdk/aws-iam'
 import * as targets from '@aws-cdk/aws-events-targets'
 import * as events from '@aws-cdk/aws-events'
 import * as glue from '@aws-cdk/aws-glue'
+import { lamdbaFirehoseCoin } from './lambdaFirehoseExchange'
 
 export class CoinCapFirehoseS3RdsStack extends cdk.Stack {
+  public readonly LamdbaFirehoseCoin: lamdbaFirehoseCoin
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     const coinCapBucket = new s3.Bucket(this, 'coinCapBucket')
 
-    const deliveryStreamRole = new iam.Role(this, 'deliveryStreamRole', {
-      assumedBy: new iam.ServicePrincipal('firehose.amazonaws.com'),
+    new LamdbaFirehoseCoin(this, 'LamdbaFirehoseCoin', {
+      rawBucket: coinCapBucket,
     })
-
-    const deliveryStreamPolicy = new iam.Policy(this, 'deliveryStreamPolicy', {
-      statements: [
-        new iam.PolicyStatement({
-          actions: [
-            's3:PutObject',
-          ],
-          resources: [
-            coinCapBucket.bucketArn + '/*',
-          ],
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            's3:ListBucket',
-          ],
-          resources: [
-            coinCapBucket.bucketArn,
-          ],
-        }),
-      ],
-    })
-    
-    deliveryStreamPolicy.attachToRole(deliveryStreamRole)
-
-    const coinCapDeliveryStream = new CfnDeliveryStream(this, 'coinCapDeliveryStream', {
-      deliveryStreamName: 'coinCapDeliveryStream',
-      extendedS3DestinationConfiguration: {
-        bucketArn: coinCapBucket.bucketArn,
-        roleArn: deliveryStreamRole.roleArn,
-        prefix: 'exchange=!{partitionKeyFromQuery:exchange}/!{timestamp:yyyy/MM/dd}/',
-        errorOutputPrefix: 'error/!{firehose:error-output-type}/',
-        bufferingHints: {
-          intervalInSeconds: 900,
-        },
-        dynamicPartitioningConfiguration: {
-          enabled: true,
-        },
-        processingConfiguration: {
-          enabled: true,
-          processors: [
-            {
-              type: 'MetadataExtraction',
-              parameters: [
-                {
-                  parameterName: 'MetadataExtractionQuery',
-                  parameterValue: '{exchange: .exchangeId}',
-                },
-                {
-                  parameterName: 'JsonParsingEngine',
-                  parameterValue: 'JQ-1.6',
-                },
-              ],
-            },
-            {
-              type: 'AppendDelimiterToRecord',
-              parameters: [
-                {
-                  parameterName: 'Delimiter',
-                  parameterValue: '\\n',
-                },
-              ],
-            },
-          ],
-        },
-      },
-    })
-
-    const lambdaToFirehoseRole = new iam.Role(this, 'lambdaToFirehoseRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
-      ],
-    })
-
-    // verbose role which allows iot-core to write to kinesis firehose delivery stream
-    const lambdaToFirehosePolicy = new iam.Policy(this, 'lambdaToFirehosePolicy', {
-      statements: [new iam.PolicyStatement({
-        actions: [
-          'firehose:PutRecord',
-        ],
-        resources: [coinCapDeliveryStream.attrArn],
-      })], 
-    })
-  
-    lambdaToFirehosePolicy.attachToRole(lambdaToFirehoseRole)
-  
-
-    const fetchData = new lambdanodejs.NodejsFunction(this, 'fetchData', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      environment: {
-        DELIVERYSTREAM_NAME: coinCapDeliveryStream.deliveryStreamName!,
-      },
-      role: lambdaToFirehoseRole,
-    })
-
-    const eventRule = new events.Rule(this, 'fetchDataScheduleRule', {
-      schedule: events.Schedule.cron({ minute: '/1' }),
-    })
-    eventRule.addTarget(new targets.LambdaFunction(fetchData))
 
     const rawdb = new glue.Database(this, 'coinCapRaw', {
       databaseName: 'coincapraw',
